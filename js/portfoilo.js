79cc6228977d8439b4f07a2a66056ea38b659d68
Chi <chiara.boisaubin@gmail.com>
2018-05-17 23:44:53 -0400

    updating the design and footer

---
+++ js/portfoilo.js
@@ -0,0 +1,10 @@
+
+//nav menu//
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+      document.getElementById("mySidenav").style.height = "auto";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
