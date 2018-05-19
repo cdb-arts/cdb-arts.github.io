c4d64f51aa7ea33547ed6d3fbbb5ff4ac88ad6ff
Chi <chiara.boisaubin@gmail.com>
2018-05-19 02:40:24 -0400

    updating the alignment for a couple of page, and changed the

--- js/portfoilo.js
+++ js/portfoilo.js
@@ -3,6 +3,7 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "100%";
       document.getElementById("mySidenav").style.height = "auto";
+      document.getElementById("mySidenav").style.margin = "0 auto";
 }
 
 function closeNav() {
